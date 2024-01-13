01c431aa8d8ed4a690048ab69a57e43ecc9923ea
Ярослав <146277976+izumiprogs@users.noreply.github.com>
2024-01-13 10:05:21 +0300

    Лаб.Работа 5 вариант сумма членов ряда

---
+++ лабраб5вар/лабраб5вар/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace лабраб5вар
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double eps = 0.001;
+            double x = double.Parse(Console.ReadLine());
+            double S = 0;
+            int N = 0;
+            double A = x;
+            do
+            {
+                S = S + A;
+                N += 1;
+                A= A*(2/(N+1));
+            }
+            while (A >= eps);
+            Console.WriteLine($"Сумма членов ряда S={S}число итераций N={N}");
+        }
+    }
+    }
+
